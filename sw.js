9f185e5e847f536a389b74b7b91e142600060d50
mutschkiepchen <festzug_insular.8x@icloud.com>
2026-03-18 06:08:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'insulin-rechner-v1';
+const URLS_TO_CACHE = ['./','./index.html','./manifest.json','./sw.js','./icon-180.png','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => {
+    if (key !== CACHE_NAME) return caches.delete(key);
+  }))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
